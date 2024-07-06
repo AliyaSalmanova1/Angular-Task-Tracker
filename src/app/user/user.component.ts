f0dd4c0c950875f26fb2c7f597e72abff7213228
Aliya Salmanova <aliyasalmanova1@gmail.com>
2024-07-06 16:00:36 -0400

    tasks for each user display

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter} from '@angular/core';
-import {DUMMY_USERS} from '../dummy-users';
+import {User} from './user.model';
+
 
 
 @Component({
@@ -8,18 +9,17 @@ import {DUMMY_USERS} from '../dummy-users';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
+
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({required: true}) user!: User;
   @Output() select = new EventEmitter<string>()
 
   get imagePath(){
-    return 'assets/users/' + this.avatar
+    return 'assets/users/' + this.user.avatar
   }
   
   onSelectUser(){
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 
 }
