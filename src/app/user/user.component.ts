4f461531370b68ec57f8cb54df72614d56e4a907
Aliya Salmanova <aliyasalmanova1@gmail.com>
2024-07-06 10:14:58 -0400

    users render on screen

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter} from '@angular/core';
+import {DUMMY_USERS} from '../dummy-users';
+
 
 @Component({
   selector: 'app-user',
@@ -7,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter<string>()
+
+  get imagePath(){
+    return 'assets/users/' + this.avatar
+  }
+  
+  onSelectUser(){
+    this.select.emit(this.id);
+  }
 
 }
